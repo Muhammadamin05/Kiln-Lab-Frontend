dafd4f1fdd87c70e09dbe5a70642f1f62eab9fa1
Muhammadamin05 <5cg85h8vrp@privaterelay.appleid.com>
2026-08-22 07:49:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import DentalLabTracker from "./DentalLabTracker.jsx";
+import DentalLabTracker from "./DentalLabTracker.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

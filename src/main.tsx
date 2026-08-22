3e7e00428ff6e95c8e5c16e3ee62716bf7d14b0e
Muhammadamin05 <5cg85h8vrp@privaterelay.appleid.com>
2026-08-22 07:31:01 +0300

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DentalLabTracker from "./DentalLabTracker.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DentalLabTracker />
+  </React.StrictMode>
+);
